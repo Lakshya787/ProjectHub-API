817ecb0041bf6b0c157ef57ce453aa77a6b6d9f6
Lakshya787 <lakshyadewangan78@gmail.com>
2026-07-11 17:44:02 +0530

    confusing import

--- Learnings/Module1/main.go
+++ Learnings/Module1/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "goproject/Learnings/module1/packages/muth"
+	"fmt"
+	"goproject/Learnings/module1/packages/muth"
+	"github.com/google/uuid"
 )
 
 func main() {
 	fmt.Println("my first word")
 	fmt.Printf("addition using math package: %v\n", muth.Add(1, 2))
+	fmt.Println(uuid.New().String())
 }
