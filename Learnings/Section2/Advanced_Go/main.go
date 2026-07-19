70a9dd29cc757f37bcdb4546e62d9fc2bbb06fa7
Lakshya787 <lakshyadewangan78@gmail.com>
2026-07-19 16:21:36 +0530

    strmet

--- Learnings/Section2/Advanced_Go/main.go
+++ Learnings/Section2/Advanced_Go/main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello World")
 	fmt.Println("Sorry")
 	fmt.Println("76")
+	strmet()
 }
--- Learnings/Section2/Advanced_Go/strmet.go
+++ Learnings/Section2/Advanced_Go/strmet.go
@@ -1,5 +1,25 @@
 package main
 
-func strmet()  {
-	
+import "fmt"
+
+type Car struct {
+	Name  string
+	Type  string
+	Brand string
+	Years int
+}
+
+//Method Years
+func (c Car) operational(){
+	if c.Years>15{
+		fmt.Printf("Car with name %s and Brand %s is not operational",c.Name,c.Brand)
+	}else{
+				fmt.Printf("Car with name %s and Brand %s and %v years of operation is left",c.Name,c.Brand,15-c.Years)
+	}
+}
+
+func strmet() {
+	c := Car{"Model S", "EV", "Tesla", 3}
+	fmt.Printf("Name of the car: %s\n",c.Name)
+	c.operational()
 }
